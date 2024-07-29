7a8709a9446244fd66965a014edc524f8bb4dd9f
Angelapeters1601 <nwattaha@gmail.com>
2024-07-29 22:24:32 +0200

    corrected some errors

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,13 +1,26 @@
 import confetti from "./assets/confetti.gif";
+import React, { useState, useEffect } from "react";
 
 function Result({ isWinner }) {
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    if (isWinner) {
+      setShowConfetti(true);
+      const timeout = setTimeout(() => {
+        setShowConfetti(false);
+      }, 5000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isWinner]);
+
   return (
     <div className="result">
-      {isWinner && (
+      {showConfetti && (
         <>
-          <h2>Congratulations, You won 🎉</h2>
-          <img src={confetti} />
-          {window.alert("refresh to play again")}
+          <img src={confetti} alt="Confetti" />
+          <h2>Congratulations, You win 🎉</h2>
         </>
       )}
     </div>
